1f87312faca30c0b2c7cd3c2c1d4bbd938937c0a
masashi-tamaki <masashi.tamaki@bacallan.com>
2022-06-18 18:26:10 +0900

    ゲームクリア表示を追加

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    GameObject image_game_clear;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image_game_clear = GameObject.Find("ImageGameClear");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (image_game_clear.activeSelf)
+        {
+            image_game_clear.SetActive(false);
+        }
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Blocks");
+        if (blocks.Length == 1)
+        {
+            image_game_clear.SetActive(true);
+        }
         Destroy(this.gameObject);
+
     }
 
 }
